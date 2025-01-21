9aa8e35f5776a4f6cf71a0dc394a76dd72f941de
alirezadoostdar <alireza.doostdar@gmail.com>
2025-01-21 16:46:00 +0100

    add user control for text box

--- WPFTest/Form1.xaml.cs
+++ WPFTest/Form1.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,22 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WPFTest
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Form1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Form1 : Window
     {
-        public bool running;
-        public MainWindow()
+        public Form1()
         {
             InitializeComponent();
-
         }
-
     }
 }
