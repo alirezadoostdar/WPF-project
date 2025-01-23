87b404754aba9b9dc5bbc82058f6435abd337939
alirezadoostdar <alireza.doostdar@gmail.com>
2025-01-23 19:19:38 +0100

    add 11 section folder browser

---
+++ 11_FolderBrowserDialog/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using WinForms = System.Windows.Forms;
+using System.Windows;
+
+
+namespace _11_FolderBrowserDialog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFire_Click(object sender, RoutedEventArgs e)
+        {
+            WinForms.FolderBrowserDialog folderBrowser = new WinForms.FolderBrowserDialog();
+            folderBrowser.ShowDialog();
+            WinForms.DialogResult result = folderBrowser.ShowDialog();
+
+            if (result ==WinForms.DialogResult.OK)
+            {
+                string folder = folderBrowser.SelectedPath;
+            }
+        }
+    }
+}
