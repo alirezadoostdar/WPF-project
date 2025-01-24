ab01c1664cc851bab8d889e748d1959a88dfedd5
alirezadoostdar <alireza.doostdar@gmail.com>
2025-01-24 13:29:24 +0100

    add section 14 project

---
+++ 13_ListView2_ObsevableColection/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _13_ListView2_ObsevableColection
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
