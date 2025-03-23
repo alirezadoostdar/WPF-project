0ec180544ae7413914faa5c0ff639bce5feaa6a4
alirezadoostdar <alireza.doostdar@gmail.com>
2025-03-23 22:31:57 +0100

    add error handling

--- WPFTest/App.xaml.cs
+++ WPFTest/App.xaml.cs
@@ -9,6 +9,11 @@ namespace WPFTest
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message, "Error ");
+            e.Handled = true;
+        }
     }
 
 }
---
+++ WPFTest/HandelingException.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFTest
+{
+    /// <summary>
+    /// Interaction logic for HandelingException.xaml
+    /// </summary>
+    public partial class HandelingException : Window
+    {
+        public HandelingException()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSum_Click(object sender, RoutedEventArgs e)
+        {
+            int a = Convert.ToInt32(txtNumber1.Text);
+            int b = Convert.ToInt32(txtNumber2.Text);
+            int sum = a + b;
+            txtSum.Text = sum.ToString();
+        }
+    }
+}
