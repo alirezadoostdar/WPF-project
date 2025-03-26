1c1f784058bf181ff9cd8877a74bb793d2147cc4
alirezadoostdar <alireza.doostdar@gmail.com>
2025-03-26 13:01:17 +0100

    add validation form p1

---
+++ WPFTest/Validations/ValidationRule.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFTest.Validations
+{
+    /// <summary>
+    /// Interaction logic for ValidationRule.xaml
+    /// </summary>
+    public partial class ValidationRule : Window
+    {
+        public ValidationRule()
+        {
+            InitializeComponent();
+            DataContext = new Customer();
+        }
+
+        private void btnSvae_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("customer save");
+        }
+    }
+}
