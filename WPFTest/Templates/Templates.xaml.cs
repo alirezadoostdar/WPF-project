53692f63f467738a650f6c920f4dd09f6e43f2c2
alirezadoostdar <alireza.doostdar@gmail.com>
2025-03-26 14:57:49 +0100

    add control template for buton

---
+++ WPFTest/Templates/Templates.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFTest.Templates
+{
+    /// <summary>
+    /// Interaction logic for Templates.xaml
+    /// </summary>
+    public partial class Templates : Window
+    {
+        public Templates()
+        {
+            InitializeComponent();
+        }
+
+        private void btnControlTemplate_Click(object sender, RoutedEventArgs e)
+        {
+            var win = new ControlTemplate();
+            win.Owner = this;
+            win.ShowDialog();
+        }
+    }
+}
