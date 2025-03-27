6c74ce06ec0f03ebedfe9427469fcafac0dc994e
alirezadoostdar <alireza.doostdar@gmail.com>
2025-03-27 11:35:42 +0100

    Add Hierarchical templeta for menu Item

---
+++ WPFTest/Templates/MyMenuItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFTest.Templates;
+
+public class MyMenuItem
+{
+    public MyMenuItem(string item)
+    {
+        Item = item;
+        Items = new ObservableCollection<MyMenuItem>();
+    }
+    public string Item { get; set; }
+    public ObservableCollection<MyMenuItem> Items{ get; set; }
+
+    public static ObservableCollection<MyMenuItem> DefaultMenu()
+    {
+        var list = new ObservableCollection<MyMenuItem>();
+        var file = new MyMenuItem("File");
+        file.Items.Add(new MyMenuItem("New"));
+        file.Items.Add(new MyMenuItem("Save"));
+        file.Items.Add(new MyMenuItem("Svae as.."));
+        file.Items.Add(new MyMenuItem("Exit"));
+
+        var edit = new MyMenuItem("Edit");
+        edit.Items.Add(new MyMenuItem("Copy"));
+        edit.Items.Add(new MyMenuItem("Cut"));
+        edit.Items.Add(new MyMenuItem("Paste"));
+
+        list.Add(file);
+        list.Add(edit);
+
+        return list;
+    }
+}
--- WPFTest/Templates/Templates.xaml.cs
+++ WPFTest/Templates/Templates.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+
 
 namespace WPFTest.Templates
 {
@@ -34,6 +23,13 @@ namespace WPFTest.Templates
         private void btnDataTemplate_Click(object sender, RoutedEventArgs e)
         {
             var win  = new DataTemplate();
+            win.Owner = this;
+            win.ShowDialog();
+        }
+
+        private void btnHierarchicalTemplate_Click(object sender, RoutedEventArgs e)
+        {
+            var win = new HierarchicalTemplate();
             win.Owner = this;
             win.ShowDialog();
         }
